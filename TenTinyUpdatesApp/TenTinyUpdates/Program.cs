a86c7cae9eca16fcb29be2b3480449b974dce045
Tim Corey <tim@iamtimcorey.com>
2024-02-22 12:31:41 -0600

    Alias Any Type

--- TenTinyUpdatesApp/TenTinyUpdates/Program.cs
+++ TenTinyUpdatesApp/TenTinyUpdates/Program.cs
@@ -1,4 +1,13 @@
 ﻿
+// #3 - Alias any type
+using TenTinyUpdates; // Normal
+using static System.Console; // More recent, but not new
+using Person = (string firstName, string lastName); // New
+
+Person person = ("Tim", "Corey");
+PrimaryConstructors p = new(person.firstName, person.lastName);
+WriteLine(person);
+
 
 // #2 - Collection Expression
 List<string> names = ["Tim", "Sue", "Bob"];
@@ -7,4 +16,4 @@ List<string> moreNames = ["Greg", "Mary", "Pam"];
 // Including the spread operator (..)
 string[] combo = [.. names, .. moreNames];
 
-Console.ReadLine();
+ReadLine();
