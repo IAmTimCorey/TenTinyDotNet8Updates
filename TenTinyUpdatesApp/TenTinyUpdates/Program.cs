072ef7503f52db071166a2ee05d39bce55d94589
Tim Corey <tim@iamtimcorey.com>
2024-02-22 12:25:27 -0600

    Collection Expression

--- TenTinyUpdatesApp/TenTinyUpdates/Program.cs
+++ TenTinyUpdatesApp/TenTinyUpdates/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+// #2 - Collection Expression
+List<string> names = ["Tim", "Sue", "Bob"];
+List<string> moreNames = ["Greg", "Mary", "Pam"];
+
+// Including the spread operator (..)
+string[] combo = [.. names, .. moreNames];
+
+Console.ReadLine();
