8d1b82febacb47024eef7c84edae971609688b07
Tim Corey <tim@iamtimcorey.com>
2024-02-22 13:06:27 -0600

    OpenFolderDialog in WPF

---
+++ TenTinyUpdatesApp/WpfDemo/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace WpfDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFolder_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFolderDialog dialog = new()
+            {
+                Title = "Select a folder",
+                InitialDirectory = @"C:\Demos",
+                AddToRecent = false,
+                RootDirectory = @"C:\Demos"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                MessageBox.Show($"You selected {dialog.FolderName}");
+            }
+        }
+    }
+}
