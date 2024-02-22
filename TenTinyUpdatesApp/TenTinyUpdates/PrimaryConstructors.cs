7ef15984a60a820ee18d8844f52ef67a4e452df3
Tim Corey <tim@iamtimcorey.com>
2024-02-22 12:20:07 -0600

    Primary Constructors

---
+++ TenTinyUpdatesApp/TenTinyUpdates/PrimaryConstructors.cs
@@ -0,0 +1,16 @@
+﻿namespace TenTinyUpdates;
+
+public class PrimaryConstructors(string firstName, string lastName)
+{
+    // You can still make them readonly or change their names
+    private readonly string _firstName = firstName;
+
+    // You can force them to be readonly
+    private readonly string lastName = lastName;
+
+    // You can still use regular constructors
+    public PrimaryConstructors() : this("Tim", "Corey")
+    {
+
+    }
+}
