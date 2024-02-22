6e67835dd83212e40c68c6146458deedeb6d54be
Tim Corey <tim@iamtimcorey.com>
2024-02-22 14:50:28 -0600

    New Validations

---
+++ TenTinyUpdatesApp/TenTinyUpdates/PersonToValidate.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TenTinyUpdates;
+
+public class PersonToValidate
+{
+    [Length(2, 10)]
+    public required string FirstName { get; set; }
+
+    [Range(0, 120, MinimumIsExclusive = false, MaximumIsExclusive = true)]
+    public int Age { get; set; }
+}
